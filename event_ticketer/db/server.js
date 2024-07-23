defb0bef7763eb01544d057abf16c15e18cb8958
jrosan9 <jbrosan4@gmail.com>
2024-07-23 13:52:42 -0400

    change to server.js

--- event_ticketer/db/server.js
+++ event_ticketer/db/server.js
@@ -7,9 +7,9 @@ app.use(cors());
 const { parseToken } = require("../middleware/utils.js");
 const port = process.env.PORT || 5050;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
 app.use(parseToken);
 app.use("/api/events", require("../Routes/events"));
 app.use("/api/categories", require("../Routes/categories"));
@@ -17,6 +17,8 @@ app.use("/api/venues", require("../Routes/venues"));
 app.use("/api/auth", require("../Routes/auth"));
 app.use("/api/reviews", require("../Routes/reviews"));
 
+app.use("/", express.static(__dirname + "/../client/dist"));
+app.use("*", express.static(__dirname + "/../client/dist"));
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
