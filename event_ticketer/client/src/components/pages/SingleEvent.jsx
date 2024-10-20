6bf8a22312abbb9608268fe4bc13eef42f8e65fa
jrosan9 <jbrosan4@gmail.com>
2024-10-20 15:18:54 -0400

    changes to file

--- event_ticketer/client/src/components/pages/SingleEvent.jsx
+++ event_ticketer/client/src/components/pages/SingleEvent.jsx
@@ -734,6 +734,7 @@ import ArchiveIcon from "@mui/icons-material/Archive";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Link } from "react-router-dom";
 
 function SingleEvent() {
   const events = useSelector((state) => state.events);
