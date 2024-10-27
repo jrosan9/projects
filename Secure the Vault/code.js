e1457f68bb2c2d4d34979f02942e8a44eefdbe46
jrosan9 <162187373+jrosan9@users.noreply.github.com>
2024-10-27 18:05:42 -0400

    Delete Secure the Vault directory

--- Secure the Vault/code.js
+++
@@ -1,8 +0,0 @@
-const message =
-  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
-
-let num1 = 8 + 2;
-let num2 = 10 * 4;
-let num3 = 41 - 2;
-//  this alert function will show up at the top of browser, allowing user to identify what the combination is
-alert(message + " " + num1 + " " + num2 + " " + num3);
